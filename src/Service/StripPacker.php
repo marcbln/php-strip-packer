0e492ec25f5a9907586582573af9910cf345c417
Marc Christenfeldt <marc.christenfeldt@gmx.de>
2021-03-15 23:50:41 +0100

    refactor

--- src/Service/StripPacker.php
+++ src/Service/StripPacker.php
@@ -50,11 +50,11 @@ class StripPacker
      *
      * 03/2021 created
      *
-     * @param int $width
+     * @param int $containerWidth
      * @param Box[] $boxes
      * @param string $sortBy
      */
-    public function pack(int $width, array $boxes, array $sortBy)
+    public function pack(int $containerWidth, array $boxes, array $sortBy)
     {
         $this->remainingBoxes = $boxes; // remaining = deepcopy(rectangles)
         Assertion::allInArray(array_keys($sortBy), ['width', 'depth', 'footprintArea', 'footprintCircumference', 'longestFootprintEdge'], 'The algorithm only supports sorting by width, depth, footprintArea, footprintCircumference, longestFootprintEdge');
@@ -75,12 +75,12 @@ class StripPacker
         while (!empty($this->remainingBoxes)) {
             $firstBox = array_shift($this->remainingBoxes);
             // UtilDebug::d("next box at next level..." . $firstBox->getFootprintAsString());
-            if ($firstBox->getDepth() <= $width) {
+            if ($firstBox->getDepth() <= $containerWidth) {
                 // UtilDebug::d("rotate...");
                 $firstBox->rotateFootprint90Degrees();
             }
             $this->packedBoxes[] = PackedBox::create($firstBox, $x, $y, 0);
-            [$x, $y, $w, $h, $DEPTH] = [$firstBox->getWidth(), $DEPTH, $width - $firstBox->getWidth(), $firstBox->getDepth(), $DEPTH + $firstBox->getDepth()];
+            [$x, $y, $w, $h, $DEPTH] = [$firstBox->getWidth(), $DEPTH, $containerWidth - $firstBox->getWidth(), $firstBox->getDepth(), $DEPTH + $firstBox->getDepth()];
 
             $this->recursive_packing($x, $y, $w, $h);
             [$x, $y] = [0, $DEPTH];
@@ -101,62 +101,57 @@ class StripPacker
         # for each unplaced item...
         $wd = ['width', 'depth'];
         foreach ($this->remainingBoxes as $idx => $box) { //    for idx in indices:
-            foreach ([0, 1] as $j) { // assuming D==1 //        for j in range(0, D + 1):
-                if ($priority > 1 && $box->getLengthByName($wd[(0 + $j) % 2]) == $w && $box->getLengthByName($wd[(1 + $j) % 2]) == $h) {
-                    [$priority, $orientation, $bestIdx] = [1, $j, $idx];
+            foreach ([0, 1] as $orientation) { // assuming D==1 //        for orientation in range(0, D + 1):
+                if ($priority > 1 && $box->getLengthByName($wd[(0 + $orientation) % 2]) == $w && $box->getLengthByName($wd[(1 + $orientation) % 2]) == $h) {
+                    [$priority, $bestOrientation, $bestIdx] = [1, $orientation, $idx];
                     break; // we found a perfect match
-                } elseif ($priority > 2 and $box->getLengthByName($wd[(0 + $j) % 2]) == $w and $box->getLengthByName($wd[(1 + $j) % 2]) < $h) {
-                    [$priority, $orientation, $bestIdx] = [2, $j, $idx];
-                } elseif ($priority > 3 and $box->getLengthByName($wd[(0 + $j) % 2]) < $w and $box->getLengthByName($wd[(1 + $j) % 2]) == $h) {
-                    [$priority, $orientation, $bestIdx] = [3, $j, $idx];
-                } elseif ($priority > 4 and $box->getLengthByName($wd[(0 + $j) % 2]) < $w and $box->getLengthByName($wd[(1 + $j) % 2]) < $h) {
-                    [$priority, $orientation, $bestIdx] = [4, $j, $idx];
+                } elseif ($priority > 2 and $box->getLengthByName($wd[(0 + $orientation) % 2]) == $w and $box->getLengthByName($wd[(1 + $orientation) % 2]) < $h) {
+                    [$priority, $bestOrientation, $bestIdx] = [2, $orientation, $idx];
+                } elseif ($priority > 3 and $box->getLengthByName($wd[(0 + $orientation) % 2]) < $w and $box->getLengthByName($wd[(1 + $orientation) % 2]) == $h) {
+                    [$priority, $bestOrientation, $bestIdx] = [3, $orientation, $idx];
+                } elseif ($priority > 4 and $box->getLengthByName($wd[(0 + $orientation) % 2]) < $w and $box->getLengthByName($wd[(1 + $orientation) % 2]) < $h) {
+                    [$priority, $bestOrientation, $bestIdx] = [4, $orientation, $idx];
                 } elseif ($priority > 5) {
-                    [$priority, $orientation, $bestIdx] = [5, $j, $idx];
+                    // nothing found
+                    [$priority, $bestOrientation, $bestIdx] = [5, $orientation, $idx];
                 }
             }
         }
-
-        // UtilDebug::d("priority: $priority");
 
         if ($priority < 5) {
 
             // ---- add best box to result
             $bestBox = $this->remainingBoxes[$bestIdx];
-            if($orientation == 1) {
+            if ($bestOrientation == 1) {
                 $bestBox->rotateFootprint90Degrees();
             }
-            [$omega, $d] = [$bestBox->getWidth(), $bestBox->getDepth()];
 
             $this->packedBoxes[] = PackedBox::create($bestBox, $x, $y, 0);
             unset($this->remainingBoxes[$bestIdx]); //        indices.remove(bestIdx)
 
-            //
-            if ($priority == 2) {
-                $this->recursive_packing($x, $y + $d, $w, $h - $d);
+
+            if ($priority == 1) {
+                // fitted perfectly - we are done
+            } elseif ($priority == 2) {
+                $this->recursive_packing($x, $y + $bestBox->getDepth(), $w, $h - $bestBox->getDepth());
             } elseif ($priority == 3) {
-                $this->recursive_packing($x + $omega, $y, $w - $omega, $h);
+                $this->recursive_packing($x + $bestBox->getWidth(), $y, $w - $bestBox->getWidth(), $h);
             } elseif ($priority == 4) {
-                // -- find minWidth / minDepth
-                $min_w = PHP_INT_MAX; // sys.maxsize
-                $min_h = PHP_INT_MAX; // sys.maxsize
+                // -- find minWidth / minDepth of remaining boxes
+                $minLengthOfRemaining = PHP_INT_MAX; // sys.maxsize
                 foreach ($this->remainingBoxes as $box) {
-                    $min_w = min($min_w, $box->getWidth());
-                    $min_h = min($min_h, $box->getDepth());
+                    $minLengthOfRemaining = min($minLengthOfRemaining, $box->getWidth(), $box->getDepth());
                 }
-                # Because we can rotate:
-                $min_w = min($min_h, $min_w);
-                $min_h = $min_w;
-                if ($w - $omega < $min_w) {
-                    $this->recursive_packing($x, $y + $d, $w, $h - $d);
-                } elseif ($h - $d < $min_h) {
-                    $this->recursive_packing($x + $omega, $y, $w - $omega, $h);
-                } elseif ($omega < $min_w) {
-                    $this->recursive_packing($x + $omega, $y, $w - $omega, $d);
-                    $this->recursive_packing($x, $y + $d, $w, $h - $d);
+                if ($w - $bestBox->getWidth() < $minLengthOfRemaining) {
+                    $this->recursive_packing($x, $y + $bestBox->getDepth(), $w, $h - $bestBox->getDepth());
+                } elseif ($h - $bestBox->getDepth() < $minLengthOfRemaining) {
+                    $this->recursive_packing($x + $bestBox->getWidth(), $y, $w - $bestBox->getWidth(), $h);
+                } elseif ($bestBox->getWidth() < $minLengthOfRemaining) {
+                    $this->recursive_packing($x + $bestBox->getWidth(), $y, $w - $bestBox->getWidth(), $bestBox->getDepth());
+                    $this->recursive_packing($x, $y + $bestBox->getDepth(), $w, $h - $bestBox->getDepth());
                 } else {
-                    $this->recursive_packing($x, $y + $d, $omega, $h - $d);
-                    $this->recursive_packing($x + $omega, $y, $w - $omega, $h);
+                    $this->recursive_packing($x, $y + $bestBox->getDepth(), $bestBox->getWidth(), $h - $bestBox->getDepth());
+                    $this->recursive_packing($x + $bestBox->getWidth(), $y, $w - $bestBox->getWidth(), $h);
                 }
             }
         }
@@ -167,55 +162,3 @@ class StripPacker
 
 
 
-//def recursive_packing(x, y, w, h, D, remaining, indices, result):
-//    """
-//    Helper function to recursively fit a certain area.
-//    D = 0 (no rotation allowed) or 1 (rotation allowed)
-//    "remaining" = list or ALL boxes
-//    """
-//    logger.info(f'recursive_packing({x}, {y}, {w}, {h}, {D}, {indices}, {result})')
-//    priority = 6
-//    for idx in indices:
-//        for j in range(0, D + 1):
-//            if priority > 1 and $box->getLengthByName($wd[(0 + $j) % 2]) == w and $box->getLengthByName($wd[(1 + $j) % 2]) == h:
-//                priority, orientation, best = 1, j, idx
-//                break
-//            elif priority > 2 and $box->getLengthByName($wd[(0 + $j) % 2]) == w and $box->getLengthByName($wd[(1 + $j) % 2]) < h:
-//                priority, orientation, best = 2, j, idx
-//            elif priority > 3 and $box->getLengthByName($wd[(0 + $j) % 2]) < w and $box->getLengthByName($wd[(1 + $j) % 2]) == h:
-//                priority, orientation, best = 3, j, idx
-//            elif priority > 4 and $box->getLengthByName($wd[(0 + $j) % 2]) < w and $box->getLengthByName($wd[(1 + $j) % 2]) < h:
-//                priority, orientation, best = 4, j, idx
-//            elif priority > 5:
-//                priority, orientation, best = 5, j, idx
-//    if priority < 5:
-//        if orientation == 0:
-//            omega, d = remaining[best][0], remaining[best][1]
-//        else:
-//            omega, d = remaining[best][1], remaining[best][0]
-//        result[best] = Rectangle(x, y, omega, d)
-//        indices.remove(best)
-//        if priority == 2:
-//            recursive_packing(x, y + d, w, h - d, D, remaining, indices, result)
-//        elif priority == 3:
-//            recursive_packing(x + omega, y, w - omega, h, D, remaining, indices, result)
-//        elif priority == 4:
-//            min_w = sys.maxsize
-//            min_h = sys.maxsize
-//            for idx in indices:
-//                min_w = min(min_w, remaining[idx][0])
-//                min_h = min(min_h, remaining[idx][1])
-//            # Because we can rotate:
-//            min_w = min(min_h, min_w)
-//            min_h = min_w
-//            if w - omega < min_w:
-//                recursive_packing(x, y + d, w, h - d, D, remaining, indices, result)
-//            elif h - d < min_h:
-//                recursive_packing(x + omega, y, w - omega, h, D, remaining, indices, result)
-//            elif omega < min_w:
-//                recursive_packing(x + omega, y, w - omega, d, D, remaining, indices, result)
-//                recursive_packing(x, y + d, w, h - d, D, remaining, indices, result)
-//            else:
-//                recursive_packing(x, y + d, omega, h - d, D, remaining, indices, result)
-//                recursive_packing(x + omega, y, w - omega, h, D, remaining, indices, result)
-//
